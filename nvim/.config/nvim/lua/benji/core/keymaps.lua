a241c1757958f440b32cf018379d7cbb74a19a85
Benjámin Bank <benjaminbank@gmail.com>
2023-07-30 17:14:06 +0200

    adds search/replace selected text to keymaps, adds format to keymaps

--- nvim/.config/nvim/lua/benji/core/keymaps.lua
+++ nvim/.config/nvim/lua/benji/core/keymaps.lua
@@ -8,11 +8,11 @@ vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv') -- Move selected text down in visu
 vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv') -- Move selected text up in visual mode
 
 -- Keep cursor in the middle / at the same position
-vim.keymap.set('n', 'J', 'mzJ`z') -- Moving the next line to behind current line
+vim.keymap.set('n', 'J', 'mzJ`z')       -- Moving the next line to behind current line
 vim.keymap.set('n', '<C-d>', '<C-d>zz') -- Page down
 vim.keymap.set('n', '<C-u>', '<C-u>zz') -- Page up
-vim.keymap.set('n', 'n', 'nzzzv') -- Keep search terms in the middle
-vim.keymap.set('n', 'N', 'Nzzzv') -- Keep search terms in the middle
+vim.keymap.set('n', 'n', 'nzzzv')       -- Keep search terms in the middle
+vim.keymap.set('n', 'N', 'Nzzzv')       -- Keep search terms in the middle
 
 -- Keep copied buffer when overwriting/deleting
 vim.keymap.set('x', '<leader>p', [['_dP]])
@@ -24,3 +24,10 @@ vim.keymap.set('n', '<leader>Y', [['+Y]])
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- vim.keymap.set('n', 'Q', '<nop>')
+-- vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+-- Finds and replaces all instances of selected word (like Cmd+Shift+G in VS Code)
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
